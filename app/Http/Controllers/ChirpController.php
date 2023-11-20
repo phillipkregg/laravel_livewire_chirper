39668fe1b5afc830059e7c8c6cf46eb9a23444be
Phil Lackey <plackey@rxante.com>
2023-11-19 20:01:32 -0600

    Initialized project and made livewire components to handle creation and listing of Chirps

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps', [
+
+        ]);
+    }
+}
